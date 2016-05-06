9dcd0fdfa7d9ee9d87ceccec0e92c7084642378a
matthew <m5w@users.noreply.github.com>
2016-05-06 13:54:02 -0400

    Commit initial work before rewrite

--- bacterium.py
+++ bacterium.py
@@ -1,26 +1,49 @@
 class Bacterium(object):
-    """ a is the inradius' square's coefficient in the formula for surface
-    area, since _surface_area_inradius_square_coefficient is too long a
-    variable name to use. """
-    def __init__(self, surface_area_a, inradius, volume_a):
-        self._surface_area_is_cached = False
-        self._surface_area_a = surface_area_a
-        self._inradius = inradius
-        self._volume_is_cached = False
-        self._volume_a = volume_a
+    def __init__(self, s_r_2_a, r, v_r_3_a, min_atp_r_3_a, s_atp,
+            fission_atp_r_2_a, fission_atp_r_3_a):
+        self._s_is_cached = False
+        self._s_r_2_a = s_r_2_a
+        self._r = r
+        self._v_is_cached = False
+        self._v_r_3_a = v_r_3_a
+        self._min_atp_is_cached = False
+        self._min_atp_r_3_a = min_atp_r_3_a
+        self._fission_atp_is_cached = False
+        self._s_atp = s_atp
+        self._fission_atp_r_2_a = fission_atp_r_2_a
+        self._fission_atp_r_3_a = fission_atp_r_3_a
 
-    def _return_surface_area(self):
-        if self._surface_area_is_cached:
-            return self._surface_area
+    def _get_s(self):
+        if self._s_is_cached:
+            return self._s
 
-        self._surface_area = self._a * self._inradius ** 2
-        self._surface_area_is_cached = True
-        return self._surface_area
+        self._s = self._s_r_2_a * self._r ** 2
+        self._s_is_cached = True
+        return self._s
 
-    def _return_volume(self):
-        if self._volume_is_cached:
-            return self._volume
+    def _get_v(self):
+        if self._v_is_cached:
+            return self._v
 
-        self._volume = self._inradius ** 3
-        self._volume_is_cached = True
-        return self._volume
+        self._v = self._v_r_3_a * self._r ** 3
+        self._v_is_cached = True
+        return self._v
+
+    def _get_min_atp(self):
+        if self._min_atp_is_cached:
+            return self._min_atp
+
+        self._min_atp = self._min_atp_r_3_a * self._get_v()
+        self._min_atp_is_cached = True
+        return self._min_atp
+
+    def _get_fission_atp(self):
+        if self._fission_atp_is_cached:
+            return self._fission_atp
+
+        self._fission_atp = (self._s_atp + self._fission_atp_r_2_a *
+                self._get_s() + self._fission_atp_r_3_a * self._get_v())
+        self._fission_atp_is_cached = True
+        return self._fission_atp
+
+    def t(self, p):
