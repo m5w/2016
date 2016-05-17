6b6826bfda53eec67be05950a6018172db1da765
matthew <m5w@users.noreply.github.com>
2016-05-16 23:51:32 -0400

    Add binary fission with the BacteriumBinaryFission exception

--- main.py
+++ main.py
@@ -31,12 +31,20 @@ def get_substrate_consumption_rate_monod(YIELD_COEFFICIENT, specific_growth_rate
 
     return substrate_consumption_rate_monod
 
-class BacteriumDeath:
+class BacteriumDeath: # to-do: derive this from object
     pass
 
+class BacteriumBinaryFission(object):
+    def __init__(self, bacterium):
+        self._bacterium = bacterium
+
+    def bacterium(self):
+        return self._bacterium
+
 class Bacterium(object):
-    def __init__(self, DEATH_PROBABILITY, growth_rate, substrate_consumption_rate, biomass):
+    def __init__(self, DEATH_PROBABILITY, BINARY_FISSION_BIOMASS, growth_rate, substrate_consumption_rate, biomass):
         self._DEATH_PROBABILITY = DEATH_PROBABILITY
+        self._BINARY_FISSION_BIOMASS = BINARY_FISSION_BIOMASS
         self._growth_rate = growth_rate
         self._substrate_consumption_rate = substrate_consumption_rate
         self._biomass = biomass
@@ -45,6 +53,11 @@ class Bacterium(object):
         return self._biomass
 
     def substrate_consumption_rate(self, substrate_concentration):
+        if self._biomass >= self._BINARY_FISSION_BIOMASS:
+            biomass = self._biomass
+            self._biomass /= 2.0
+            raise BacteriumBinaryFission(Bacterium(self._DEATH_PROBABILITY, self._BINARY_FISSION_BIOMASS, self._growth_rate, self.substrate_consumption_rate, biomass - self._biomass))
+
         # Since random.random() ``-> x in the interval [0, 1)" (Python 2.7 online help utility), random.random() will never be less than zero but always less than 1.
         if random.random() < self._DEATH_PROBABILITY:
             raise BacteriumDeath
@@ -67,7 +80,12 @@ class Chemostat(object):
             try:
                 self._substrate_concentration -= bacterium.substrate_consumption_rate(substrate_concentration)
                 bacteria.append(bacterium)
-            except(BacteriumDeath):
+            # to-do: research if the try block can be executed from an except block
+            # obsolete: just add the bacteria to the end
+            except BacteriumBinaryFission as bacterium_binary_fission:
+                self._bacteria.append(bacterium)
+                self._bacteria.append(bacterium_binary_fission.bacterium())
+            except(BacteriumDeath): # to-do: remove parentheses
                 pass
 
         self._bacteria = bacteria
