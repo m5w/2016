14b7b297a389aaca378f338a8115a9c6fce864d4
matthew <m5w@users.noreply.github.com>
2016-05-17 15:07:04 -0400

    Add flow and calculate concentration each time step

--- main.py
+++ main.py
@@ -3,31 +3,30 @@ import random
 import scipy.stats
 
 def get_specific_growth_rate_monod(MAXIMUM_SPECIFIC_GROWTH_RATE, SUBSTRATE_SATURATION_CONSTANT):
-    def specific_growth_rate_monod(substrate_concentration):
-        return MAXIMUM_SPECIFIC_GROWTH_RATE * substrate_concentration / (SUBSTRATE_SATURATION_CONSTANT + substrate_concentration)
+    def specific_growth_rate_monod(SUBSTRATE_CONCENTRATION):
+        return MAXIMUM_SPECIFIC_GROWTH_RATE * SUBSTRATE_CONCENTRATION / (SUBSTRATE_SATURATION_CONSTANT + SUBSTRATE_CONCENTRATION)
 
     return specific_growth_rate_monod
 
 def get_bacterium_monod(monod):
-    def bacterium_monod(substrate_concentration, bacterium):
-        return monod(substrate_concentration, bacterium.biomass())
+    def bacterium_monod(SUBSTRATE_CONCENTRATION, BACTERIUM):
+        return monod(SUBSTRATE_CONCENTRATION, BACTERIUM.biomass())
 
     return bacterium_monod
 
 def get_growth_rate_monod(specific_growth_rate_monod):
     @get_bacterium_monod
-    def growth_rate_monod(substrate_concentration, biomass):
-        return specific_growth_rate_monod(substrate_concentration) * biomass
+    def growth_rate_monod(SUBSTRATE_CONCENTRATION, BIOMASS):
+        return specific_growth_rate_monod(SUBSTRATE_CONCENTRATION) * BIOMASS
 
     return growth_rate_monod
 
 def get_substrate_consumption_rate_monod(YIELD_COEFFICIENT, specific_growth_rate_monod):
-    assert type(YIELD_COEFFICIENT) is float
     YIELD_COEFFICIENT_INVERSE = 1.0 / YIELD_COEFFICIENT
 
     @get_bacterium_monod
-    def substrate_consumption_rate_monod(substrate_concentration, biomass):
-        return YIELD_COEFFICIENT_INVERSE * specific_growth_rate_monod(substrate_concentration) * biomass
+    def substrate_consumption_rate_monod(SUBSTRATE_CONCENTRATION, biomass):
+        return YIELD_COEFFICIENT_INVERSE * specific_growth_rate_monod(SUBSTRATE_CONCENTRATION) * biomass
 
     return substrate_consumption_rate_monod
 
@@ -35,51 +34,69 @@ class BacteriumDeath(BaseException):
     pass
 
 class BacteriumBinaryFission(BaseException):
-    def __init__(self, bacterium):
-        self._bacterium = bacterium
+    def __init__(self, BACTERIUM):
+        self._BACTERIUM = BACTERIUM
 
     def bacterium(self):
-        return self._bacterium
+        return self._BACTERIUM
 
 class Bacterium(object):
-    def __init__(self, BINARY_FISSION_BIOMASS, DEATH_PROBABILITY, growth_rate, substrate_consumption_rate, biomass):
+    def __init__(self, SPECIES, BINARY_FISSION_BIOMASS, DEATH_PROBABILITY, growth_rate, substrate_consumption_rate, BIOMASS):
+        self._SPECIES = SPECIES
         self._BINARY_FISSION_BIOMASS = BINARY_FISSION_BIOMASS
         self._DEATH_PROBABILITY = DEATH_PROBABILITY
         self._growth_rate = growth_rate
         self._substrate_consumption_rate = substrate_consumption_rate
-        self._biomass = biomass
+        self._biomass = BIOMASS
 
-    def biomass(self):
+    def get_species(self):
+        return self._SPECIES
+
+    def get_biomass(self):
         return self._biomass
 
-    def substrate_consumption_rate(self, substrate_concentration):
+    def get_substrate_consumption_rate(self, SUBSTRATE_CONCENTRATION):
         if self._biomass >= self._BINARY_FISSION_BIOMASS:
-            biomass = self._biomass
+            BIOMASS = self._biomass
             self._biomass /= 2.0
-            raise BacteriumBinaryFission(Bacterium(self._BINARY_FISSION_BIOMASS, self._DEATH_PROBABILITY, self._growth_rate, self._substrate_consumption_rate, biomass - self._biomass))
+            raise BacteriumBinaryFission(Bacterium(self._BINARY_FISSION_BIOMASS, self._DEATH_PROBABILITY, self._growth_rate, self._substrate_consumption_rate, BIOMASS - self._biomass))
 
         # Since random.random() ``-> x in the interval [0, 1)" (Python 2.7 online help utility), random.random() will never be less than zero but always less than 1.
         if random.random() < self._DEATH_PROBABILITY:
             raise BacteriumDeath
 
-        substrate_consumption_rate_ = self._substrate_consumption_rate(substrate_concentration, self)
-        self._biomass += self._growth_rate(substrate_concentration, self)
-        return substrate_consumption_rate_
+        SUBSTRATE_CONSUMPTION_RATE = self._substrate_consumption_rate(SUBSTRATE_CONCENTRATION, self)
+        self._biomass += self._growth_rate(SUBSTRATE_CONCENTRATION, self)
+        return SUBSTRATE_CONSUMPTION_RATE
 
 class Chemostat(object):
-    def __init__(self, substrate_concentration):
-        self._substrate_concentration = substrate_concentration
+    def __init__(self, SUBSTRATE_CONCENTRATION, VOLUME):
+        self._substrate_concentration = SUBSTRATE_CONCENTRATION
+        self._volume = VOLUME
 
         self._bacteria = []
 
-    def get_substrate_concentration(self):
-        substrate_concentration = self._substrate_concentration
+        self._substrate_mass = self._substrate_concentration * self._volume
+
+    def extend_bacteria(self, L):
+        self._bacteria.extend(L)
+
+    def _get_substrate_concentration(self, INFLOW_SUBSTRATE_CONCENTRATION, INFLOW_VOLUME, OUTFLOW_VOLUME):
+        self._substrate_mass += INFLOW_SUBSTRATE_CONCENTRATION * INFLOW_VOLUME - self._substrate_concentration * OUTFLOW_VOLUME
+        self._volume += INFLOW_VOLUME - OUTFLOW_VOLUME
+        self._substrate_concentration = self._substrate_mass / self._volume
+        return self._substrate_concentration
+
+    def substrate_concentration(self, INFLOW_SUBSTRATE_CONCENTRATION, INFLOW_VOLUME, OUTFLOW_VOLUME):
+        SUBSTRATE_CONCENTRATION = self._get_substrate_concentration(INFLOW_SUBSTRATE_CONCENTRATION, INFLOW_VOLUME, OUTFLOW_VOLUME)
+
         bacteria = []
 
         bacterium = 0
+
         while True:
             try:
-                self._substrate_concentration -= self._bacteria[bacterium].substrate_consumption_rate(substrate_concentration)
+                self._substrate_concentration -= self._bacteria[bacterium].substrate_consumption_rate(SUBSTRATE_CONCENTRATION)
                 bacteria.append(self._bacteria[bacterium])
             except BacteriumBinaryFission as bacterium_binary_fission:
                 self._bacteria.append(self._bacteria[bacterium])
